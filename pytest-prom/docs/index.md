6527bc0f4af3e05f44fd85c48a7a3315a31a25a7
David Stark <david@starkers.org>
2019-05-21 01:20:48 +0100

    clone of pytest-prometheus

---
+++ pytest-prom/docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to pytest-prom
+
+prom metrics
---
+++ pytest-prom/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-prom',
+    version='0.0.1',
+    author='David Stark',
+    author_email='david@starkers.org',
+    maintainer='David Stark',
+    maintainer_email='david@starkers.org',
+    license='MIT',
+    url='https://github.com/starkers/pytest-prom',
+    description='prom metrics',
+    long_description=read('README.rst'),
+    py_modules=['pytest_prom'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'prom = pytest_prom',
+        ],
+    },
+)
